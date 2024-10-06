076253a5d7359cafeb22ac9389167c82ccbcfc4d
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2024-10-06 17:27:14 +0530

    Update backend url

--- src/routes/IPConfig.ts
+++ src/routes/IPConfig.ts
@@ -1,3 +1,4 @@
-export const BACKEND_URL = "https://miniurl.up.railway.app";
+export const BACKEND_URL = "https://mini-url-z2bl.onrender.com";
+// export const BACKEND_URL = "https://miniurl.up.railway.app";
 export const LOCAL_URL = "http://localhost:8001";
 export const URL = BACKEND_URL
